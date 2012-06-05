7c51f79931e8cf35afc2c0481e41e1e0df416578
Dan Borthwick <dborthwick@dborthwick-mb.(none)>
2012-06-06 00:56:20 +0100

    Pinch to zoom (non-centred)

--- CameraQuestion/CameraController.m
+++ CameraQuestion/CameraController.m
@@ -12,6 +12,9 @@
 
 @interface CameraController()
 
+- (float)distanceOfCameraFromPoleGivenAvatarToPole:(CGSize)avatarToPole andCamera:(CameraModel *)camera;
+- (CGPoint)cameraPositionGivenAvatarToPole:(CGSize)avatarToPole distanceOfCameraFromPole:(float)distanceOfCameraFromPole polePosition:(const CGPoint)polePosition;
+
 @end
 
 
@@ -20,17 +23,30 @@
 - (void)updateCamera:(CameraModel*)camera forPoleWithPosition:(const CGPoint)polePosition andAvatarWithPosition:(const CGPoint)avatarPosition
 {
     CGSize avatarToPole = [VectorGeometry vectorFrom:avatarPosition to:polePosition];
+    float distanceOfCameraFromPole = [self distanceOfCameraFromPoleGivenAvatarToPole:avatarToPole andCamera:camera];
+    CGPoint cameraPosition = [self cameraPositionGivenAvatarToPole:avatarToPole distanceOfCameraFromPole:distanceOfCameraFromPole polePosition:polePosition];
+    
+    [camera moveTo:cameraPosition];
+    [camera lookAt:polePosition];
+}
+
+- (float)distanceOfCameraFromPoleGivenAvatarToPole:(CGSize)avatarToPole andCamera:(CameraModel *)camera
+{
     float distanceOfAvatarFromPole = [VectorGeometry magnitudeOf:avatarToPole];
     float halfFieldOfView = [camera fieldOfViewInRadians] / 2.0f;
     float distanceOfCameraFromPole = 2 * distanceOfAvatarFromPole / tanf(halfFieldOfView);
     
+    return distanceOfCameraFromPole;
+}
+
+- (CGPoint)cameraPositionGivenAvatarToPole:(CGSize)avatarToPole distanceOfCameraFromPole:(float)distanceOfCameraFromPole polePosition:(const CGPoint)polePosition
+{
     CGSize vectorInDirectionFromPoleToCamera = [VectorGeometry normalTo:avatarToPole];
     CGSize unitVectorFromPoleToCamera = [VectorGeometry unitVectorInDirectionOf:vectorInDirectionFromPoleToCamera];
     CGSize poleToCamera = [VectorGeometry scale:unitVectorFromPoleToCamera by:distanceOfCameraFromPole];
     CGPoint cameraPosition = [VectorGeometry translate:polePosition by:poleToCamera];
     
-    [camera moveTo:cameraPosition];
-    [camera lookAt:polePosition];
+    return cameraPosition;
 }
 
 @end
